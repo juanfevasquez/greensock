b28da3a5b333d1e4ff76bb1c27407d9385a5c721
Juan F. Vasquez <jfvasquez@xumak.com>
2017-04-03 12:24:48 -0500

    Timelines

---
+++ js/main.js
@@ -0,0 +1,79 @@
+(function() {
+    // How to Animate one element after another
+
+    var title = document.querySelector('.header-title'),
+        intro = document.querySelector('.intro'),
+        subtitle = document.querySelector('.header-subtitle'),
+        list = document.querySelector('.list')
+        image = document.querySelector('.image');
+
+    // Use TimelineMax library: included with TweenMax
+
+    // Set a new variable for the TimelineMax
+    var tl = new TimelineMax();
+    /* 
+    tl.from(title, 0.3, {
+        y: -30,
+        autoAlpha: 0,
+        ease: Power1.easeOut,
+    })
+    .from(intro, 0.3, {
+        y: -30,
+        autoAlpha: 0,
+        ease: Power1.easeOut
+    }, '-=0.15')
+    .from(subtitle, 0.3, {
+        y: -30,
+        autoAlpha: 0,
+        ease: Power1.easeOut
+    }, '-=0.15')
+    .from(list, 0.3, {
+        y: -30,
+        autoAlpha: 0,
+        ease: Power1.easeOut
+    }, 3)
+    .fromTo(image, 2, {
+        scale: 0,
+        ease: Bounce.easeInOut
+    },
+    {
+        scale: 0.5
+    });
+    */
+
+    // How to use labels
+    // What is a label: is a tool that allows me to point to a time in the timeline where I want some
+    // animation to be triggered. eg:
+
+    /*
+    tl.from(title, 0.6, {
+        y: -30,
+        autoAlpha: 0,
+        ease: Power1.easeOut,
+    })
+    .add('start_image_animation')
+    .from(intro, 1, {
+        y: -30,
+        autoAlpha: 0,
+        ease: Power1.easeOut
+    })
+    .from(subtitle, 0.3, {
+        y: -30,
+        autoAlpha: 0,
+        ease: Power1.easeOut
+    })
+    .from(list, 0.3, {
+        y: -30,
+        autoAlpha: 0,
+        ease: Power1.easeOut
+    })
+    .from(image, 0.3, {
+        autoAlpha: 0,
+        ease: Power1.easeInOut
+    }, 'start_image_animation');
+    */
+
+    //Timeline Playback
+    
+
+})();
